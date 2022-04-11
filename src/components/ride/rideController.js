2ac60246bae62181eeb7adcd60227ba12f870881
Manish <manishdarmal@gmail.com>
2022-04-11 12:09:29 +0530

    Added factory functions

--- src/components/ride/rideController.js
+++ src/components/ride/rideController.js
@@ -1,49 +1,6 @@
 import Ride from './rideModel.js';
-import { logger } from '../../middleware/logger.js';
-import { catchAsync } from '../../utils/catchAsync.js';
-import ExpressError from '../../utils/ExpressError.js';
+import * as factory from './services/factoryController.js';
 
-export const createRides = catchAsync(async (req, res) => {
-  const { ride, driverName, location } = req.body;
-  const newRide = await Ride.create({ ride, driverName, location });
+export const createRide = factory.createNewRide(Ride);
 
-  res.json({
-    status: 'Success',
-    data: {
-      newRide,
-    },
-  });
-});
-
-export const getNearbyRides = catchAsync(async (req, res) => {
-  const { distance, latlng } = req.params;
-  const [lat, lng] = latlng.split(','); //19.117659, 72.863165
-
-  // Calulating radius based on 'KM'
-  const radius = distance / 6378.1;
-
-  if (!lat || !lng) {
-    throw new Error(
-      'Please provide latitude and longitude in the format lat, lng'
-    );
-  }
-
-  logger.info(distance, lat, lng);
-
-  const rides = await Ride.find({
-    location: {
-      $geoWithin: {
-        $centerSphere: [[lng, lat], radius],
-      },
-    },
-  });
-
-  if (rides.length === 0)
-    throw new ExpressError('No nearby rides available!!', 404);
-
-  res.status(200).json({
-    status: 'Success',
-    result: rides.length,
-    data: rides,
-  });
-});
+export const getNearbyRides = factory.getRides(Ride);
