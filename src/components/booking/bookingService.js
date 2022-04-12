cb701da77081ad95271297a9de3ae9fed0f6be00
Manish <manishdarmal@gmail.com>
2022-04-12 18:27:25 +0530

    Updated bookingService.js

--- src/components/booking/bookingService.js
+++ src/components/booking/bookingService.js
@@ -1,60 +1,49 @@
 import { logger } from '../../middleware/logger.js';
 import { catchAsync } from '../../utils/catchAsync.js';
 import ExpressError from '../../utils/ExpressError.js';
+import Ride from '../ride/rideModel.js';
+import Booking from './bookingModel.js';
 
-export const bookOne = (Model_1, Model_2) =>
-  catchAsync(async (req, res) => {
-    const { startLocation, destination } = req.body;
+export const bookOneRide = async (startLocation, destination, userId) => {
+  logger.info('inside bookOneRide service');
+  const radius = 5 / 6378.1;
 
-    const radius = 5 / 6378.1;
+  const [lng, lat] = startLocation.coordinates.map((el) => el);
 
-    const [lng, lat] = startLocation.coordinates.map((el) => el);
+  logger.info(lng);
+  logger.info(lat);
 
-    logger.info(lng);
-    logger.info(lat);
-
-    const ride = await Model_1.find({
-      isBooked: false,
-      location: {
-        $geoWithin: {
-          $centerSphere: [[lng, lat], radius],
-        },
+  const ride = await Ride.find({
+    isBooked: false,
+    location: {
+      $geoWithin: {
+        $centerSphere: [[lng, lat], radius],
       },
-    });
-
-    if (ride.length === 0) {
-      throw new ExpressError('Uh oh! No rides nearby.', 401);
-    }
-    const nearestRide = ride[0].id;
-    logger.info(ride);
-
-    const bookedRide = await Model_2.create({
-      startLocation,
-      destination,
-      isConfrimed: true,
-      ride: nearestRide,
-      user: req.user.id,
-    });
-
-    res.status(200).json({
-      staus: 'success',
-      data: {
-        bookedRide,
-      },
-    });
+    },
   });
 
-export const getAll = (Model) =>
-  catchAsync(async (req, res) => {
-    const bookings = await Model.find({ user: req.user.id });
+  if (ride.length < 1) {
+    throw new ExpressError('Uh oh! No rides nearby.', 401);
+  }
+  const nearestRide = ride[0].id;
 
-    if (bookings.length === 0)
-      throw new ExpressError('You have not done any bookings yet!', 404);
-
-    res.status(200).json({
-      status: 'success',
-      data: {
-        bookings,
-      },
-    });
+  const bookedRide = await Booking.create({
+    startLocation,
+    destination,
+    isConfrimed: true,
+    ride: nearestRide,
+    user: userId,
   });
+
+  return bookedRide;
+};
+
+export const getUserBookings = async (userId) => {
+  logger.info('inside getUserBookings service');
+  const bookings = await Booking.find({ user: userId });
+
+  if (bookings.length < 1)
+    throw new ExpressError('You have not done any bookings yet!', 404);
+
+  return bookings;
+};
