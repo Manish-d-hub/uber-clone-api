b0e42dca8125952bb2243604ba4eef4814524254
Manish <manishdarmal@gmail.com>
2022-04-12 19:05:47 +0530

    Updated rideService.js

--- src/components/ride/rideService.js
+++ src/components/ride/rideService.js
@@ -1,50 +1,42 @@
 import { logger } from '../../middleware/logger.js';
-import { catchAsync } from '../../utils/catchAsync.js';
 import ExpressError from '../../utils/ExpressError.js';
+import Ride from './rideModel.js';
 
-export const createNewRide = (Model) =>
-  catchAsync(async (req, res) => {
-    const { ride, driverName, location } = req.body;
-    const newRide = await Model.create({ ride, driverName, location });
+export const createNewRide = async (rideType, driverName, location) => {
+  logger.info('Inside createNewRide service');
+  const newRide = await Ride.create({ rideType, driverName, location });
 
-    res.json({
-      status: 'Success',
-      data: {
-        newRide,
+  if (!newRide)
+    throw new ExpressError('Uh oh! server down, try again later', 500);
+
+  return newRide;
+};
+
+export const getRides = async (distance, latlng) => {
+  logger.info('Inside getRides service');
+  const [lat, lng] = latlng.split(','); //19.117659, 72.863165
+
+  // Calulating radius based on 'KM'
+  const radius = distance / 6378.1;
+
+  if (!lat || !lng) {
+    throw new Error(
+      'Please provide latitude and longitude in the format lat, lng'
+    );
+  }
+
+  logger.info(distance, lat, lng);
+
+  const rides = await Ride.find({
+    location: {
+      $geoWithin: {
+        $centerSphere: [[lng, lat], radius],
       },
-    });
+    },
   });
 
-export const getRides = (Model) =>
-  catchAsync(async (req, res) => {
-    const { distance, latlng } = req.params;
-    const [lat, lng] = latlng.split(','); //19.117659, 72.863165
+  if (rides.length < 1)
+    throw new ExpressError('No nearby rides available!!', 404);
 
-    // Calulating radius based on 'KM'
-    const radius = distance / 6378.1;
-
-    if (!lat || !lng) {
-      throw new Error(
-        'Please provide latitude and longitude in the format lat, lng'
-      );
-    }
-
-    logger.info(distance, lat, lng);
-
-    const rides = await Model.find({
-      location: {
-        $geoWithin: {
-          $centerSphere: [[lng, lat], radius],
-        },
-      },
-    });
-
-    if (rides.length === 0)
-      throw new ExpressError('No nearby rides available!!', 404);
-
-    res.status(200).json({
-      status: 'Success',
-      result: rides.length,
-      data: rides,
-    });
-  });
+  return rides;
+};
